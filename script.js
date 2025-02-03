f4e7632f42d288a0c6b0aee16387026db76707bd
Tobias <tobimation23@gmail.com>
2025-02-03 13:24:44 +0100

    opgave 1

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let person = {
+gender: "male",
+height: "5'8",
+pet: "lizard",
+single: "yeah",
+
+}
+
+
+
+console.log(person.gender) // male
+console.log(person.height) // 5'8
+console.log(person["pet"]) // lizard
+console.log(person["single"]) //yeah
